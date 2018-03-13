0b3bc44f1f8b220f42587c2dbd3de4f4ca8622d3
Spen Hanley <b00330712@studentmail.uws.ac.uk>
2018-03-13 15:44:19 +0000

    Updated scripts file to allow for dynamic creation of form fields

--- sha_training_app/static/js/scripts.js
+++ sha_training_app/static/js/scripts.js
@@ -1,9 +1,10 @@
 function createFields()
 {
   var numberOfQuestions = document.getElementById('no_questions').value
+  var questionBase = document.getElementById('')
 
   for (var i = 0; i < numberOfQuestions; i++)
   {
-    
+
   }
 }
